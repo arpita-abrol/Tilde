31bb5a3b8bcae749001af946760512c98228df13
yabaschu <stanleyzeng1998@gmail.com>
2016-03-10 23:43:37 -0500

    Finish MergeSortTester stanley

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -25,11 +25,11 @@ This results in O(n(log(n)).
 
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
+  n=1       time: 5955.34 nanoseconds
+  n=10      time: 6521.12 nanoseconds
+  n=100     time: 21956 .05 nanoseconds
   ...
-  n=<huge>  time: 
+  n=<huge>  time: 224717.12 nanoseconds
 
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
@@ -49,6 +49,49 @@ public class MergeSortTester
     public static void main( String[] args ) 
     {
 
-    }//end main
+  public static void main( String[] args ){ 
+    
+        
+    double a = mergeTester(1,100);
+    double b = mergeTester(10,100);
+    double c = mergeTester(100,100);
+    double d = mergeTester(1000,100);
+    System.out.println("The time it takes to sort 1 elements it " +a+" nanoseconds");
+    System.out.println("The time it takes to sort 10 elements it " +b+" nanoseconds");
+    System.out.println("The time it takes to sort 100 elements it " +c+" nanoseconds");
+    System.out.println("The time it takes to sort 1000 elements it " +d+" nanoseconds");
+     
+
+  
+}//end main
+
+public static int[] makeList(int listLength){
+  int[] temp = new int[listLength];
+  for (int x = 0; x < listLength ; x ++){
+    temp[x]=(int)(Math.random()*(1001));
+    
+  }
+  return temp;
+}
+
+public static double mergeTester(int listLength, int a){
+  int[] temp=new int[listLength];
+  int y=0;
+  double first;
+  double time=0.0;
+  double end;
+  while (y < a){
+    temp = makeList(listLength);
+    first = System.nanoTime();
+    MergeSort.sort(temp);
+    time += System.nanoTime()-first;
+    y++;
+    
+  }
+  end = accu/ ((double)a);
+  return end;
+  
+  
+}
 
 }//end class
